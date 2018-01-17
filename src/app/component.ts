70d8e803002727820c84a1140def666503b3ef8c
Chaim Greenberg <chaim8greenberg@gmail.com>
2018-01-17 14:12:23 +0200

    qwefqwe

--- src/app/component.ts
+++ src/app/component.ts
@@ -1,3 +1,4 @@
+
 import { ApplicationRef, Component } from "@angular/core";
 import { Model } from "./repository.model";
 import { Product } from "./product.model";
@@ -9,7 +10,7 @@ export class ProductComponent {
     model: Model = new Model();
     getProduct(key: number): Product {
         return this.model.getProduct(key);
-    }
+}
     getProducts(): Product[] {
         return this.model.getProducts();
 }
@@ -19,5 +20,28 @@ export class ProductComponent {
 }
     addProduct(p: Product) {
         console.log("New Product: " + this.jsonProduct);
+}
+    getValidationMessages(state: any, thingName?: string) {
+        let thing: string = state.path || thingName;
+        let messages: string[] = [];
+        if (state.errors) {
+            for (let errorName in state.errors) {
+                switch (errorName) {
+                    case "required":
+                        messages.push(`А чо  ${thing} Пушкин вводить будет?`);
+                        break;
+                    case "minlength":
+                        messages.push(` Опять ${thing} ....хотя бы 
+                            ${state.errors['minlength'].requiredLength}
+                            символов ввести надо`);
+                            break;
+    case "pattern":
+        messages.push(`в ${thing} ты ввел какую-то дичь
+             `);
+break;
+                }
+            }
+        }
+        return messages;
     }
 }
